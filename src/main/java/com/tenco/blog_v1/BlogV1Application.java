bc540aae5228a40dd5e23602eb01e9a260c91aab
wjdgns5 <rhfovkeh5@naver.com>
2024-10-07 16:35:32 +0900

    create- 익명게시판 만들기

--- src/main/java/com/tenco/blog_v1/BlogV1Application.java
+++ src/main/java/com/tenco/blog_v1/BlogV1Application.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BlogV1Application {
-
 	public static void main(String[] args) {
+
 		SpringApplication.run(BlogV1Application.class, args);
 	}
-
 }
